8db53d590d2c5c259585afae758bdacbefefd409
Yan Khamutouski <yan_kh28@mail.ru>
2018-02-24 01:06:17 +0300

    Created .jar executable. Updated Javadoc.

--- java-lab1/src/by/bsuir/Khamutouski/Main.java
+++ java-lab1/src/by/bsuir/Khamutouski/Main.java
@@ -1,10 +1,3 @@
-/**
- * This is the package of BSUIR student at
- * Computer Systems and Networks Faculty
- *
- * Cross - Platforming - Programming 2018
- *
- * */
 package by.bsuir.Khamutouski;
 
 import javafx.application.Application;
